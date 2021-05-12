af3c94302e005623b07bd953f1a13ad6e1b85245
Dan Ogrodnik <dan.ogrodnik@gmail.com>
2021-05-11 23:33:50 -0700

    changed cta wording, pop out personal website, removed link on logo

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by <a href="http://www.danogrodnik.com">Dan Ogrodnik</a>. All right reserved</div>
+            <div className="footer-copyright">Made by <a target= "blank_" href="http://www.danogrodnik.com">Dan Ogrodnik</a>. All right reserved</div>
           </div>
         </div>
       </div>
