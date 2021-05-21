6f9a59827937f5e09d454d339805d9f0934a62ea
Dan Ogrodnik <dan.ogrodnik@gmail.com>
2021-05-20 20:10:02 -0700

    updated insta

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -18,7 +18,7 @@ const FooterSocial = ({
     >
       <ul className="list-reset">
         <li>
-          <a href="https://facebook.com/">
+          <a target="blank_" href="https://facebook.com/">
             <svg
               width="16"
               height="16"
@@ -31,7 +31,7 @@ const FooterSocial = ({
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/">
+          <a target="blank_" href="https://twitter.com/">
             <svg
               width="16"
               height="16"
@@ -44,7 +44,7 @@ const FooterSocial = ({
           </a>
         </li>
         <li>
-          <a href="https://google.com/">
+          <a target="blank_" href="http://www.instagram.com/gp_electric_co">
             <svg
               width="16"
               height="16"
