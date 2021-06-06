f6c2846285bb7a9e9518f60badf1e4a0c62c7b0c
Dan Ogrodnik <dan.ogrodnik@gmail.com>
2021-06-06 15:07:10 -0700

    added emailjs to cta

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
 import Input from '../elements/Input';
+import emailjs from 'emailjs-com'; 
 
 const propTypes = {
   ...SectionProps.types,
@@ -25,6 +26,19 @@ const Cta = ({
   split,
   ...props
 }) => {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_2mscrzb', 'template_vfh4nds', e.target, 'user_xQ5xnIdUSTvusjiwooT12')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+      alert('Your email address was sent! We will be in touch shortly.')
+  }
 
   const outerClasses = classNames(
     'cta section center-content-mobile reveal-from-bottom',
@@ -57,12 +71,17 @@ const Cta = ({
               </h3>
               <p>Si Habla Español y Inglés!</p>
           </div>
-          <div className="cta-action">
-            <Input id="contact" type="email" label="Email" labelHidden hasIcon="right" placeholder="Your best email">
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
-              </svg>
-            </Input>
+          <div className="cta">
+            <form onSubmit={sendEmail}>
+            <fieldset>
+              <div class="has-icon-right">
+                <input type="submit" id="contactEmail" name="contactEmail" required type="email" class="form-input" placeholder="Your best email"/>
+                <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9"></path>
+                </svg>
+              </div>
+            </fieldset>
+            </form>
           </div>
         </div>
       </div>
